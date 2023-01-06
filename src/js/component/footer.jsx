c8c273463a379dcb244c14e9e812c22c6f1740c4
escg91 <elianacg17@gmail.com>
2023-01-06 20:12:07 +0000

    react

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,12 @@
+
+import React from "react"
+
+const Footer = function(){
+    return(
+
+<div className="text-center p-4" style={{backgroundColor: "gray"}}>
+    © 2023 Copyright.
+  </div>
+    )
+}
+export default Footer
